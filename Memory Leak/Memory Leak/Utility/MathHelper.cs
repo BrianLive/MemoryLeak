da75fecae437605dab206c40f8bec7bf85a8afdb
Dlaor <dlaory@gmail.com>
2012-11-06 21:06:44 +0100

    MathHelper added for your average Math functions

---
+++ Memory Leak/Memory Leak/Utility/MathHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MemoryLeak.Utility
+{
+    class MathHelper
+    {
+        public static float Lerp(float value, float min, float max)
+        {
+            return min + value * (max - min);
+        }
+
+        public static float Clamp(float value, float min, float max)
+        {
+            return Math.Min(Math.Max(value, min), max);
+        }
+    }
+}
