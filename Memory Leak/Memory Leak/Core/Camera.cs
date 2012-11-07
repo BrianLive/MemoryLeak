1323d143405d76d5da1f66043fbd474c1a99a9a7
Brian Benson <brian.live@live.com>
2012-11-06 22:18:27 -0900

    Fluid Camera (jk)
    Fluid Camera's are gay.

--- Memory Leak/Memory Leak/Core/Camera.cs
+++ Memory Leak/Memory Leak/Core/Camera.cs
@@ -7,11 +7,9 @@ namespace MemoryLeak.Core
 	{
 		private Matrix _matrix;
 		private Vector2 _position = Vector2.Zero;
-		private Vector2 _actualPosition = Vector2.Zero;
 		private float _rotation;
 		private float _zoom = 1;
 		private bool _isDirty = true;
-		private Vector2 _velocity = Vector2.Zero;
 
 		public Vector2 Position
 		{
@@ -19,16 +17,6 @@ namespace MemoryLeak.Core
 			set
 			{
 				_position = value;
-				_isDirty = true;
-			}
-		}
-
-		public Vector2 ActualPosition
-		{
-			get { return _actualPosition; }
-			private set
-			{
-				_actualPosition = value;
 				_isDirty = true;
 			}
 		}
@@ -57,37 +45,15 @@ namespace MemoryLeak.Core
 		{
 			get
 			{
-				if (ActualPosition != _position)
-				{
-					float acceleration = (Math.Abs(ActualPosition.X - _position.X) + Math.Abs(ActualPosition.Y - _position.Y))/2;
-
-					_velocity += LengthDir(Direction(ActualPosition, _position), acceleration);
-
-					ActualPosition += _velocity;
-
-					Console.WriteLine(ActualPosition);
-				}
-
 				if (_isDirty)
 				{
-					_matrix = Matrix.CreateTranslation(new Vector3(-ActualPosition.X, -ActualPosition.Y, 0)) *
+                    _matrix = Matrix.CreateTranslation(new Vector3(-(float)Math.Round(_position.X), -(float)Math.Round(_position.Y), 0)) *
 							  Matrix.CreateRotationZ(_rotation) * Matrix.CreateScale(new Vector3(_zoom, _zoom, 1)) *
 							  Matrix.CreateTranslation(new Vector3(Game.Core.Resolution.X * 0.5f, Game.Core.Resolution.Y * 0.5f, 0));
 				}
 
 				return _matrix;
 			}
-		}
-
-		private static float Direction(Vector2 p1, Vector2 p2)
-		{
-			float r = (float)Math.Atan2(p1.Y - p2.Y, p2.X - p1.X);
-			return r < 0 ? r + (2 * (float)Math.PI) : r;
-		}
-
-		private static Vector2 LengthDir(float dir, float len)
-		{
-			return new Vector2((float)Math.Cos(dir) * len, (float)-Math.Sin(dir) * len);
 		}
 	}
 }
