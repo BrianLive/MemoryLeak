5413b7e40b4b85f17c4baeafa7657c363983d15c
Dlaor <dlaory@gmail.com>
2012-11-08 19:05:46 +0100

    Cleaning up Utility.cs (and testing if my branch works)

--- Memory Leak/Memory Leak/Utility/Utility.cs
+++ Memory Leak/Memory Leak/Utility/Utility.cs
@@ -9,20 +9,18 @@ namespace MemoryLeak.Utility
     {
 		public static float Direction(this Vector2 p1, Vector2 p2)
 		{
-			float r = (float)Math.Atan2(p1.Y - p2.Y, p2.X - p1.X);
-			return r < 0 ? r + (2 * (float)Math.PI) : r;
+		    float r = (float)Math.Atan2(p1.Y - p2.Y, p2.X - p1.X);
+		    return r < 0 ? r + (2 * (float)Math.PI) : r;
 		}
 
 		public static Vector2 LengthDir(float dir, float len)
 		{
-			return new Vector2((float)Math.Cos(dir) * len, (float)-Math.Sin(dir) * len);
+		    return new Vector2((float)Math.Cos(dir) * len, (float)-Math.Sin(dir) * len);
 		}
 
         public static float GetSmallest(List<float> values)
         {
-            float[] smallest = {float.MaxValue};
-            foreach (var i in values.Where(i => Math.Abs(i) < Math.Abs(smallest[0]))) smallest[0] = i;
-            return smallest[0];
+            return values.Min(); //Welp... might as well remove this method now
         }
     }
 }
