19c984f9a19ac1039937cbf7a328db08bd37969f
Brian Benson <brian.live@live.com>
2012-11-08 17:09:46 -0900

    Regions
    Class Made, thats about it.

---
+++ Memory Leak/Memory Leak/Core/Region.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace MemoryLeak.Core
+{
+    public class Region
+    {
+        public RectangleF Area { get; set; }
+
+        public Region(int x, int y, int width, int height)
+        {
+            Area = new RectangleF(x*Chunk.Tile.Width, y*Chunk.Tile.Height, width*Chunk.Tile.Width,
+                                  height*Chunk.Tile.Height);
+        }
+
+
+    }
+}
