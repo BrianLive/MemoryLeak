c39f924a61bc4c71c593cd6861c18743b549d9a5
Brian Benson <brian.live@live.com>
2012-11-11 12:45:31 -0900

    Comments
    // yep.

--- Memory Leak/Memory Leak/Core/Chunk.cs
+++ Memory Leak/Memory Leak/Core/Chunk.cs
@@ -13,22 +13,69 @@ namespace MemoryLeak.Core
     {
         public class Tile : Drawable
         {
+            /// <summary>
+            /// The common width of tiles.
+            /// </summary>
             public new const float Width = 32;
+
+            /// <summary>
+            /// The common height of tiles.
+            /// </summary>
             public new const float Height = 32;
 
+            /// <summary>
+            /// Decides if entities can pass through this tile.
+            /// </summary>
             public bool IsPassable { get; set; }
+
+            /// <summary>
+            /// Decides if entities can use this tile as a ramp.
+            /// </summary>
             public bool IsRamp { get; set; }
+
+            /// <summary>
+            /// Decides the direction of the ramp.
+            /// </summary>
             public bool IsRampHorizontal { get; set; }
+
+            /// <summary>
+            /// Decides which direction of the ramp goes up and down.
+            /// </summary>
             public bool IsRampUpNegative { get; set; }
+
+            /// <summary>
+            /// Decides if the tile is visible when the player is below it.
+            /// </summary>
             public bool IsFloater { get; set; }
+
+            /// <summary>
+            /// Decides if the block should disappear or not when the player is on the layer the block is.
+            /// </summary>
             public bool IsFloaterLayered { get; set; }
             
+            /// <summary>
+            /// The Chunk that this tile belongs to.
+            /// </summary>
             public Chunk Parent { get; set; }
 
+            /// <summary>
+            /// A list of entities that are currently touching this tile.
+            /// </summary>
             public List<Entity> Children = new List<Entity>();
 
+            /// <summary>
+            /// The event that is called when an entity touches this tile.
+            /// </summary>
             public event Action<Physical> Step;
 
+            /// <summary>
+            /// Tile's are the foundation of any level.
+            /// </summary>
+            /// <param name="texture">The texture atlas that the tile will use.</param>
+            /// <param name="x">The x position of the source rectangle.</param>
+            /// <param name="y">The y position of the source rectangle</param>
+            /// <param name="width">The width of the source rectangle.</param>
+            /// <param name="height">The height of the source rectangle.</param>
             public Tile(Texture2D texture, int x = 0, int y = 0, int width = -1, int height = -1)
                 : base(texture, x, y, width, height)
             {
@@ -40,6 +87,11 @@ namespace MemoryLeak.Core
                 IsFloaterLayered = false;
             }
 
+            /// <summary>
+            /// Called when an entity touches a tile.
+            /// This method is required for ramps to work.
+            /// </summary>
+            /// <param name="sender">The entity that touched the tile.</param>
             public void OnStep(Physical sender)
             {
                 var startDepth = sender.Depth;
@@ -125,15 +177,42 @@ namespace MemoryLeak.Core
             }
         }
 
+        /// <summary>
+        /// Bi-Dimensional Array of tiles.
+        /// </summary>
         private readonly Tile[,,] _tiles;
+
+        /// <summary>
+        /// List of entities contained in the chunk.
+        /// </summary>
         private readonly List<Entity> _entities = new List<Entity>();
 
+        /// <summary>
+        /// The width of the chunk (in tiles).
+        /// </summary>
         public int Width { get; private set; }
+
+        /// <summary>
+        /// The height of the chunk (in tiles).
+        /// </summary>
         public int Height { get; private set; }
+
+        /// <summary>
+        /// The amount of layers in the chunk.
+        /// </summary>
         public int Depth { get; private set; }
 
+        /// <summary>
+        /// The state that this chunk belongs to.
+        /// </summary>
         public State Parent { get; set; }
 
+        /// <summary>
+        /// Chunks are regions of land that the player explores.
+        /// </summary>
+        /// <param name="width">The width of the map (in tiles).</param>
+        /// <param name="height">The height of the map (in tiles).</param>
+        /// <param name="depth">The amount of layers in the chunk.</param>
         public Chunk(int width, int height, int depth = 1)
         {
             Width = width;
@@ -147,6 +226,10 @@ namespace MemoryLeak.Core
                     _tiles[x, y, 0] = null;
         }
 
+        /// <summary>
+        /// Adds an entity to the chunk.
+        /// </summary>
+        /// <param name="entity">The entity to be added.</param>
         public void Add(Entity entity)
         {
             entity.Parent = this;
@@ -156,11 +239,22 @@ namespace MemoryLeak.Core
                 ((Physical)entity).CorrectParent();
         }
 
+        /// <summary>
+        /// Removes an entity from the chunk.
+        /// </summary>
+        /// <param name="entity">The entity to be removed.</param>
         public void Remove(Entity entity)
         {
             _entities.Remove(entity);
         }
 
+        /// <summary>
+        /// Sets a tile to another tile.
+        /// </summary>
+        /// <param name="x">The x location on the chunk (in tiles).</param>
+        /// <param name="y">The y location on the chunk (in tiles).</param>
+        /// <param name="z">The layer on the chunk.</param>
+        /// <param name="tile">The tile to be swapped in.</param>
         public void Set(int x, int y, int z, Tile tile)
         {
             x = Math.Max(0, Math.Min(x, Width - 1));
@@ -175,12 +269,25 @@ namespace MemoryLeak.Core
             tile.Parent = this;
         }
 
+        /// <summary>
+        /// Retrieves a tile from the tile list.
+        /// </summary>
+        /// <param name="x">The x location on the chunk (in tiles).</param>
+        /// <param name="y">The y location on the chunk (in tiles).</param>
+        /// <param name="z">The layer on the chunk.</param>
+        /// <returns>A tile.</returns>
         public Tile Get(int x, int y, int z)
         {
             if (x < Width && y < Height && z < Depth && x >= 0 && y >= 0 && z >= 0) return _tiles[x, y, z];
             return null;
         }
 
+        /// <summary>
+        /// Retrieves rectangles near a specified rectangle.
+        /// </summary>
+        /// <param name="rect">The base rectangle to expand and check around.</param>
+        /// <param name="depth">The layer that is being checked.</param>
+        /// <returns>The list of nearby tile rectangles.</returns>
         public List<RectangleF> GetNearbyRects(RectangleF rect, float depth)
         {
             var tiles = new List<RectangleF>();
@@ -205,6 +312,13 @@ namespace MemoryLeak.Core
             return tiles;
         }
 
+        /// <summary>
+        /// Checks whether or not a rectangle is colliding with impassible objects.
+        /// </summary>
+        /// <param name="sender">The object being checked against.</param>
+        /// <param name="rect">The temporary rectangle from that object.</param>
+        /// <param name="depth">The layer that is being checked.</param>
+        /// <returns>Whether or not the requested location is free.</returns>
         public bool PlaceFree(Physical sender, RectangleF rect, float depth)
         {
             foreach(var rectangle in GetNearbyRects(rect, depth))
@@ -244,6 +358,10 @@ namespace MemoryLeak.Core
             return true;
         }
 
+        /// <summary>
+        /// Draws all entities and tiles belonging to the chunk.
+        /// </summary>
+        /// <param name="spriteBatch">The spritebatch to add to.</param>
         public void Draw(SpriteBatch spriteBatch)
         {
             int playerDepth = (int) Parent.Player.Depth + 1;
@@ -269,6 +387,10 @@ namespace MemoryLeak.Core
             }
         }
 
+        /// <summary>
+        /// Updates all the entities belonging to the chunk.
+        /// </summary>
+        /// <param name="delta">The difference in time between this frame and the last.</param>
         public void Update(float delta)
         {
             foreach (var i in _entities)
